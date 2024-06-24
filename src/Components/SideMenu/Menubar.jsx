bdb6c3b93f2a504205d994f676c07bdc7a5ad3ed
Sinan-muhammed <sinasinan418@gmail.com>
2024-06-25 01:51:27 +0530

    set the menu buttons on active mode

--- src/Components/SideMenu/Menubar.jsx
+++ src/Components/SideMenu/Menubar.jsx
@@ -12,71 +12,82 @@ import { TbSettingsCog } from "react-icons/tb";
 import { TbMessages } from "react-icons/tb";
 
 const Menubar = () => {
+
+   // State to toggle the visibility of the menu
   const [hide, setHide] = useState(false);
+
+  // State to button click and set the active button
+  const [onclick,setOnClick] = useState(1)
+
   return (
     <div className="relative z-[999]">
       <div
         className={`${
           hide === true
-            ? "translate-x-[-90px]  transition-transform duration-500 ease-in-out "
-            : "translate-x-[0px] menu h-[100vh] w-[full] shadow-lg shadow-[#24292E] transform transition-transform duration-500 ease-in-out rounded-br-[45px] rounded-tr-[45px]"
+            ? "translate-x-[-350px]  transition-transform duration-1000 ease-in-out "
+            : "translate-x-[0px] menu h-[100vh] w-[full] shadow-lg shadow-[#24292E] transform transition-transform duration-1000 ease-in-out rounded-br-[45px] rounded-tr-[45px]"
         }`}
       >
+        {/* Logo section */}
         <div className="h-[10%] w-[100%] bg-slate-50 rounded-tr-[45px] flex items-center justify-center pt-[30px]">
             <img src="../../../public/orangeHrm.png" alt="" className="w-[250px]"/>
         </div>
+
+        {/* Search section */}
         <div className="h-[12%] w-[100%] bg-white flex items-end  border-b pb-4">
-             <div className=" flex gap-[15px] items-center  ml-[55px]">
+             <div className=" flex gap-[15px] items-center  pl-[55px]">
                 <LuSearch className="rotate-90 font-bold text-[25px] text-[#7F97B7]"/>
                 <h1 className="font-bold mt-[3px] text-[18px] text-[#7F97B7]"> Search</h1>
              </div>
         </div>
-        <div className="h-[80%] w-[100%] bg-slate-50 rounded-br-[45px] pt-[24px]">
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-               <BsPersonLinesFill className="text-[25px] text-[#7F97B7] font-bold "/>
-               <h1 className="font-bold text-[#7F97B7]">Admin</h1>
+
+        {/* Menu items */}
+        <div className="h-[80%] w-[100%] bg-slate-50 rounded-br-[45px] pt-[24px] flex flex-col ">
+            <div onClick={()=>setOnClick(1)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%]  rounded-r-[50px] ${onclick === 1 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' }`}>
+               <BsPersonLinesFill className="text-[25px]  font-bold "/>
+               <h1 className="font-bold ">Admin</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <MdGroups className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">PIM</h1>
+            <div onClick={()=>setOnClick(2)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 2 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' }`}>
+                <MdGroups className="text-[25px]  font-bold "/>
+                <h1 className="font-bold ">PIM</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <FcLeave className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">Leave</h1>
+            <div onClick={()=>setOnClick(3)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 3 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' }`}>
+                <FcLeave className="text-[25px]  font-bold "/>
+                <h1 className="font-bold ">Leave</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <MdAccessTime className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">Time</h1>
+            <div onClick={()=>setOnClick(4)} className={`flex items-center gap-[25px] cursor-pointer  pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 4 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' }`}>
+                <MdAccessTime className="text-[25px]  font-bold "/>
+                <h1 className="font-bold ">Time</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <GrContactInfo className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">My Info</h1>
+            <div onClick={()=>setOnClick(5)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 5 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' }`}>
+                <GrContactInfo className="text-[25px] font-bold "/>
+                <h1 className="font-bold ">My Info</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <GoHome className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">Dashboard</h1>
+            <div onClick={()=>setOnClick(6)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 6 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' }`}>
+                <GoHome className="text-[25px]  font-bold "/>
+                <h1 className="font-bold ">Dashboard</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <TbAddressBook className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">Directory</h1>
+            <div onClick={()=>setOnClick(7)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 7 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' } `}>
+                <TbAddressBook className="text-[25px]  font-bold "/>
+                <h1 className="font-bold ">Directory</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <TbSettingsCog className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">Maintenance</h1>
+            <div onClick={()=>setOnClick(8)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 8 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]' }`}>
+                <TbSettingsCog className="text-[25px]  font-bold "/>
+                <h1 className="font-bold ">Maintenance</h1>
             </div>
-            <div className="flex items-center gap-[15px]  ml-[55px]">
-                <TbMessages className="text-[25px] text-[#7F97B7] font-bold "/>
-                <h1 className="font-bold text-[#7F97B7]">Buzz</h1>
+            <div onClick={()=>setOnClick(9)} className={`flex items-center gap-[25px] cursor-pointer pl-[55px] h-[54px] w-[95%] rounded-r-[50px] ${onclick === 9 ? 'bg-gradient-to-r from-[#FB840C] to-[#F35C17] text-white' : 'bg-none text-[#7F97B7]'}`}>
+                <TbMessages className="text-[25px]  font-bold "/>
+                <h1 className="font-bold ">Buzz</h1>
             </div>
         </div>
       </div>
       <div>
         <button
           onClick={() => setHide((prev) => !prev)}
-          className={`flex text-[15px] font-semibold text-white h-[30px] w-[30px] bg-orange-600 items-center justify-center absolute   rounded-full ${
+          className={`flex text-[15px] font-semibold text-white h-[30px] w-[30px] bg-orange-600 items-center justify-center absolute  transform transition-transform duration-1000  rounded-full ${
             hide === false
               ? "right-[-14px] top-[132px]"
-              : "left-[15px] top-[130px] rotate-180"
+              : "left-[15px] top-[130px] rotate-180 "
           }`}
         >
           <FaChevronLeft />
